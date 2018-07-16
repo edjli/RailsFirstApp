d07d4fd84b4f86e93828cb66f1461ade1cc1c7f4
Edward Li <edjli@ucsc.edu>
2018-07-16 12:09:03 -0700

    4.2 changed price to decimal

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_16_181601) do
+ActiveRecord::Schema.define(version: 2018_07_16_185022) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 2018_07_16_181601) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "colour"
-    t.string "price"
+    t.decimal "price"
   end
 
 end
