25380951cceba1e67fb962e0eb542e538ab7ce7e
Edward Li <edjli@ucsc.edu>
2018-07-26 10:44:55 -0700

    4.7

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   get 'simple_pages/contact'
   get 'simple_pages/index'
   root 'simple_pages#landing_page'
-  
+
+  get 'products/index'
+
   resources :orders, only: [:index, :show, :create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
