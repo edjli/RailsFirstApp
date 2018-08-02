26192054c0996986de9f86c83b8280f6f5ea05ec
Edward Li <edjli@ucsc.edu>
2018-08-02 12:56:41 -0700

    5.7 added comments and helper scope functions that ordered desc and asc

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,10 @@ class Product < ApplicationRecord
   def self.search(search_term)
       Product.where("name LIKE ?", "%#{search_term}%")
   end
+  def highest_rating_comment
+    comments.rating_desc.first
+  end
+  def lowest_rating_comment
+    comments.rating_asc.first
+  end
 end
